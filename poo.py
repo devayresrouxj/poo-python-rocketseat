a678584e1da0b1766dc88437d0cd5d752cfc7999
Ayres Roux <devayresrouxj@gmail.com>
2025-05-20 20:57:04 -0300

    aula: o que é POO - parte 2

--- poo.py
+++ poo.py
@@ -3,3 +3,15 @@ class Pessoa:
   def __init__(self, nome, idade) -> None:
     self.nome = nome
     self.idade = idade
+  
+  def saudacao(self):
+    return f"Olá, meu nome é {self.nome} e eu tenho {self.idade} anos"
+
+# Objetos
+pessoa1 = Pessoa("Ayres", 31)
+mensagem = pessoa1.saudacao()
+print(mensagem)
+
+pessoa2 = Pessoa(nome="Taiane", idade=28)
+mensagem = pessoa2.saudacao()
+print(mensagem)
